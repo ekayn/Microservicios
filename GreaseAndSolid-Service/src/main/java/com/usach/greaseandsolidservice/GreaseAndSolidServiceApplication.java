711513f33bc3ed2a44df99f2063d9511c652d56d
Nicolas V <nicolas.valdes.h@usach.cl>
2023-06-19 23:36:20 -0400

    Creación de cada micro servicio

---
+++ GreaseAndSolid-Service/src/main/java/com/usach/greaseandsolidservice/GreaseAndSolidServiceApplication.java
@@ -0,0 +1,13 @@
+package com.usach.greaseandsolidservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreaseAndSolidServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreaseAndSolidServiceApplication.class, args);
+	}
+
+}
---
+++ GreaseAndSolid-Service/src/test/java/com/usach/greaseandsolidservice/GreaseAndSolidServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usach.greaseandsolidservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreaseAndSolidServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
